a9b2106fd995c7777002a790adde4b187e15c6f3
Christopher Watkins <chriswatkins7@yahoo.com>
2020-12-07 19:57:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,5 @@ sentence2 = ["Hi","again,","just","making","sure","it's","reversed!"]
 
   
 def reverse_each_word(sentence)
-   reversed = sentence.split(" ").collect do |word|
-   word.reverse
- end
-   reversed.join(" ")
- end 
+   sentence1.select {|word| word.reversed}
+end
